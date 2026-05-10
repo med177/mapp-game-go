7aa1357d329ca74fbcf315732fdbff7c7728832d
Mehmet ALTINEL <med177@gmail.com>
2026-05-11 02:36:40 +0300

    Refactor code structure for improved readability and maintainability

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
+	"image"
+	_ "image/png"
 	"log"
 
 	"mapp-game-go/internal/game"
@@ -8,6 +12,18 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/joho/godotenv"
 )
+
+//go:embed mapp_window_icon.png
+var windowIconPNG []byte
+
+func loadWindowIcon() {
+	img, _, err := image.Decode(bytes.NewReader(windowIconPNG))
+	if err != nil {
+		log.Printf("Icon decode failed: %v", err)
+		return
+	}
+	ebiten.SetWindowIcon([]image.Image{ebiten.NewImageFromImage(img)})
+}
 
 func main() {
 	// .env dosyasını yüklemeyi dene (varsa)
@@ -17,6 +33,7 @@ func main() {
 	ebiten.SetWindowSize(1920, 1080)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.MaximizeWindow()
+	loadWindowIcon()
 
 	g := game.New()
 	if err := ebiten.RunGame(g); err != nil {
